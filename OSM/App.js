acdd066e82cf797a1e44f8a48e37e330c8c00a58
Aaryan Giri <aaryangir@gmail.com>
2024-02-29 11:37:32 +0530

    Route between 2 address

--- OSM/App.js
+++ OSM/App.js
@@ -1,15 +1,81 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import DirectionsMap from './DirectionsMap';
+import React, { useState } from "react";
+import { View, StyleSheet, TextInput, TouchableOpacity, Text } from "react-native";
+import DirectionsMap from "./DirectionsMap";
+
+const geocodeAddress = (address, callback) => {
+  const NOMINATIM_BASE_URL = "https://nominatim.openstreetmap.org/search?";
+  const params = {
+    q: address,
+    format: "json",
+    addressdetails: 1,
+    polygon_geojson: 0,
+  };
+  const queryString = new URLSearchParams(params).toString();
+  const requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+
+  fetch(`${NOMINATIM_BASE_URL}${queryString}`, requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data && Array.isArray(data) && data.length > 0) {
+        const { lat, lon } = data[0];
+        const coordinates = [parseFloat(lat), parseFloat(lon)];
+        callback(coordinates);
+      } else {
+        console.error("No coordinates found for the address:", address);
+        callback(null);
+      }
+    })
+    .catch((error) => {
+      console.error("Error geocoding address:", address, error);
+      callback(null);
+    });
+};
 
 const App = () => {
-  // Define the start and end coordinates
-  const start = [28.7041, 77.1025]; // Delhi
-  const end = [19.076, 72.8777]; // Mumbai
+  const [start, setStart] = useState("New Delhi"); // Default start point Delhi
+  const [end, setEnd] = useState("Mumbai"); // Default end point Mumbai
+
+  const handleSearch = () => {
+    geocodeAddress(start, (startCoords) => {
+      geocodeAddress(end, (endCoords) => {
+        if (startCoords && endCoords) {
+          console.log("Start coordinates:", startCoords);
+          console.log("End coordinates:", endCoords);
+          setStartCoordinates(startCoords);
+          setEndCoordinates(endCoords);
+        } else {
+          console.error("Failed to obtain coordinates for start or end address.");
+        }
+      });
+    });
+  };
+
+  const [startCoordinates, setStartCoordinates] = useState([28.6139, 77.209]);
+  const [endCoordinates, setEndCoordinates] = useState([19.076, 72.8777]);
 
   return (
     <View style={styles.container}>
-      <DirectionsMap start={start} end={end} />
+      <DirectionsMap start={startCoordinates} end={endCoordinates} />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Start address"
+          value={start}
+          onChangeText={(text) => setStart(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="End address"
+          value={end}
+          onChangeText={(text) => setEnd(text)}
+        />
+        <TouchableOpacity style={styles.button} onPress={handleSearch}>
+          <Text style={styles.buttonText}>Search</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -17,6 +83,38 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  inputContainer: {
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: "#ffffff",
+    padding: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    borderTopWidth: 1,
+    borderTopColor: "#ccc",
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginHorizontal: 5,
+  },
+  button: {
+    backgroundColor: "#007bff",
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginLeft: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
   },
 });
 
--- OSM/DirectionsMap.js
+++ OSM/DirectionsMap.js
@@ -1,73 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import WebView from 'react-native-webview';
 
 const DirectionsMap = ({ start, end }) => {
-  const htmlContent = `
-    <!DOCTYPE html>
-    <html>
+  const [htmlContent, setHtmlContent] = useState('');
 
-    <head>
-      <title>Geolocation</title>
-      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css" />
-      <link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.css" />
+  useEffect(() => {
+    const generateHtmlContent = () => {
+      const newHtmlContent = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+          <title>Geolocation</title>
+          <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css" />
+          <link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.css" />
+          <style>
+            body {
+              margin: 0;
+              padding: 0;
+            }
+            #map {
+              width: 100%;
+              height: 100vh;
+            }
+          </style>
+        </head>
+        <body>
+          <div id="map"></div>
+          <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js"></script>
+          <script src="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js"></script>
+          <script>
+            var startCoords = ${JSON.stringify(start)};
+            var endCoords = ${JSON.stringify(end)};
+            var midLatitude = (startCoords[0] + endCoords[0]) / 2;
+            var midLongitude = (startCoords[1] + endCoords[1]) / 2;
+            var map = L.map('map').setView([midLatitude, midLongitude], 11);
+            var mapLink = "<a href='http://openstreetmap.org'>OpenStreetMap</a>";
+            L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+              attribution: 'Leaflet &copy; ' + mapLink + ', contribution',
+              maxZoom: 20
+            }).addTo(map);
+            var startMarker = L.marker(startCoords).addTo(map);
+            var endMarker = L.marker(endCoords).addTo(map);
+            L.Routing.control({
+              waypoints: [
+                L.latLng(startCoords[0], startCoords[1]),
+                L.latLng(endCoords[0], endCoords[1])
+              ]
+            }).addTo(map);
+          </script>
+        </body>
+        </html>
+      `;
+      setHtmlContent(newHtmlContent);
+    };
 
-      <style>
-        body {
-          margin: 0;
-          padding: 0;
-        }
-      </style>
-
-    </head>
-
-    <body>
-      <!-- Map container -->
-      <div id="map" style="width:100%; height: 100vh"></div>
-
-      <!-- Include Leaflet and Leaflet Routing Machine scripts -->
-      <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js"></script>
-      <script src="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js"></script>
-
-      <script>
-        // Starting and ending coordinates
-        var startCoords = ${JSON.stringify(start)};
-        var endCoords = ${JSON.stringify(end)};
-
-        // Calculate the midpoint between start and end coordinates
-        var midLatitude = (startCoords[0] + endCoords[0]) / 2;
-        var midLongitude = (startCoords[1] + endCoords[1]) / 2;
-
-        // Create a Leaflet map centered at the midpoint with zoom level 11
-        var map = L.map('map').setView([midLatitude, midLongitude], 11);
-
-        // Attribution for the tile layer
-        mapLink = "<a href='http://openstreetmap.org'>OpenStreetMap</a>";
-
-        // Add a tile layer to the map using OpenStreetMap tiles
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-          attribution: 'Leaflet &copy; ' + mapLink + ', contribution',
-          maxZoom: 18
-        }).addTo(map);
-
-        // Create markers for the starting and ending points and add them to the map
-        var startMarker = L.marker(startCoords).addTo(map);
-        var endMarker = L.marker(endCoords).addTo(map);
-
-        // Create a Leaflet Routing Machine control with waypoints set to the starting and ending coordinates
-        L.Routing.control({
-          waypoints: [
-            L.latLng(startCoords[0], startCoords[1]),
-            L.latLng(endCoords[0], endCoords[1])
-          ]
-        }).addTo(map);
-
-      </script>
-
-    </body>
-
-    </html>
-  `;
+    generateHtmlContent();
+  }, [start, end]);
 
   return (
     <View style={styles.container}>
